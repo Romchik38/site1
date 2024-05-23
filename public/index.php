ef88349b7611b8efac3a69b98fccc52d712ad840
Romchik38 <pomahehko.c@gmail.com>
2024-05-23 21:35:56 +0300

    added composer autoload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Romchik38\Server\TestClass;
+
 echo '<pre>';
 print_r($_SERVER);
 echo '</pre>';
@@ -8,3 +14,7 @@ echo '</pre>';
 echo '<pre>';
 print_r($_GET);
 echo '</pre>';
+
+$t1 = new TestClass();
+
+echo $t1->hello;
