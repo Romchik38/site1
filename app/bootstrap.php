039cb0c24d2b302e0d4f43880e99ed1f7d7c6385
Romchik38 <pomahehko.c@gmail.com>
2024-06-14 13:11:07 +0300

    refactor

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -14,13 +14,11 @@ $container = new Container();
 $container->add(DefaultRouterResult::class, new DefaultRouterResult(
     /** default response, headers, statusCode */
 ));
-
-$controllers = require_once(__DIR__ . '/code/Controllers/controllersList.php');
-
+$controllersList = require_once(__DIR__ . '/code/Controllers/controllersList.php');
 $container->add(
     DefaultRouter::class, new DefaultRouter(
             $container->get(DefaultRouterResult::class),
-            $controllers,
+            $controllersList,
             $container
     )
 );
