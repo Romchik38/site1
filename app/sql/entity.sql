56f600152d8885ddb6d6b1f49e4ae480a76c27a5
Romchik38 <pomahehko.c@gmail.com>
2024-07-01 21:01:06 +0300

    creating entity structure

--- app/sql/entity.sql
+++ app/sql/entity.sql
@@ -1,4 +1,4 @@
-CREATE table entity
+CREATE table entities
 (
     entity_id serial PRIMARY KEY, 
     name text NOT NULL UNIQUE
@@ -7,25 +7,17 @@ CREATE table entity
 CREATE table entity_text
 (
     field_name text PRIMARY KEY, 
-    entity_id int REFERENCES entity ( entity_id ),
-    value text NOT NULL
-);
-
-CREATE table entity_int
-(
-    field_name text PRIMARY KEY, 
-    entity_id int REFERENCES entity ( entity_id ),
-    value int NOT NULL
+    entity_id int REFERENCES entities ( entity_id ),
+    value text NOT NULL,
+    type text CHECK ( type IN ('int', 'float', 'string') )
 );
 
 --INFORMATION 
-INSERT INTO entity VALUES (1, 'Company Site1');
+INSERT INTO entities VALUES (1, 'Company Site1');
 
 INSERT INTO entity_text 
     VALUES 
-        ('email_contact_recovery', 1, 'ser@ozone.com.ua'),
-        ('email_contact_main', 1, 'office@ozone.com.ua')
+        ('email_contact_recovery', 1, 'ser@ozone.com.ua', 'string'),
+        ('email_contact_main', 1, 'office@ozone.com.ua', 'string'),
+        ('min_order_sum', 1, '100', 'int');
 ;
-
-INSERT INTO entity_int 
-    VALUES ('min_order_sum', 1, 100);
