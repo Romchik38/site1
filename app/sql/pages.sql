078eabac37c0a05c9e979d8347437343d8a93610
Romchik38 <pomahehko.c@gmail.com>
2024-07-06 13:49:29 +0300

    fixes

--- app/sql/pages.sql
+++ app/sql/pages.sql
@@ -12,4 +12,9 @@ INSERT INTO pages (name, url, content)
     ('About Page', 'about', '<p>We sell smartphones for 20 years. Located in Ukraine.</p><p>Contacts: site@site1.com</p>')
 ;
 
+-- HELPER
 SELECT * FROM pages;
+
+UPDATE pages SET content = '<p class="lead">Wellcome to our site. We are the best.</p><p>We offer next templates for your site:</p><ul class="list-unstyled"><li><a href="/media/base.html">Base template</a></li></ul>'
+    WHERE page_id = 1
+;
