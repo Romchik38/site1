167d88c434e49a83d55f696780ced382a7ab0d84
Romchik38 <pomahehko.c@gmail.com>
2024-08-02 16:43:05 +0300

    refactor

--- server/Api/Models/ModelInterface.php
+++ server/Api/Models/ModelInterface.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Romchik38\Server\Api\Models;
 
-interface ModelInterface {
+interface ModelInterface
+{
     public function getData(string $key): mixed;
     public function getAllData(): array;
+
     public function setData(string $key, mixed $value): ModelInterface;
 }
