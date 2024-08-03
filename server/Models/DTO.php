a4533dca3f4506a2ed5a73432f2a0f562d0c91e3
Romchik38 <pomahehko.c@gmail.com>
2024-08-03 16:06:18 +0300

    refactor

--- server/Models/DTO.php
+++ server/Models/DTO.php
@@ -6,19 +6,17 @@ namespace Romchik38\Server\Models;
 
 use Romchik38\Server\Api\Models\DTO\DTOInterface;
 
-class DTO implements DTOInterface {
+class DTO implements DTOInterface
+{
     protected array $data = [];
 
-    public function getData(string $key): mixed {
-        if (array_key_exists($key, $this->data) === true) {
-            return $this->data[$key];
-        }
-        return null;
+    public function getData(string $key): mixed
+    {
+        return $this->data[$key] ?? null;
     }
 
     public function getAllData(): array
     {
         return $this->data;
     }
-
 }
