86d6602e1a39630305f1b2111197542b466e725f
Romchik38 <pomahehko.c@gmail.com>
2024-08-04 21:33:25 +0300

    looks like meto to link repo is ready

--- app/code/Models/Sql/MenuToLinks/MenuToLinksRepository.php
+++ app/code/Models/Sql/MenuToLinks/MenuToLinksRepository.php
@@ -6,13 +6,16 @@ namespace Romchik38\Site1\Models\Sql\MenuToLinks;
 
 use Romchik38\Server\Api\Models\DatabaseInterface;
 use Romchik38\Server\Api\Models\ModelFactoryInterface;
+use Romchik38\Server\Models\Errors\CouldNotAddException;
+use Romchik38\Server\Models\Errors\CouldNotDeleteException;
+use Romchik38\Server\Models\Errors\CouldNotSaveException;
 use Romchik38\Server\Models\Errors\NoSuchEntityException;
 use Romchik38\Server\Models\Errors\DTO\CantCreateDTOException;
+use Romchik38\Server\Models\Errors\QueryExeption;
 use Romchik38\Site1\Api\Models\MenuToLinks\MenuToLinksIdDTOFactoryInterface;
 use Romchik38\Site1\Api\Models\MenuToLinks\MenuToLinksIdDTOInterface;
 use Romchik38\Site1\Api\Models\MenuToLinks\MenuToLinksInterface;
 use Romchik38\Site1\Api\Models\MenuToLinks\MenuToLinksRepositoryInterface;
-use Romchik38\Site1\Models\MenuToLinks\MenuToLinksIdDTOFactory;
 
 class MenuToLinksRepository implements MenuToLinksRepositoryInterface
 {
@@ -34,19 +37,10 @@ class MenuToLinksRepository implements MenuToLinksRepositoryInterface
     public function getById(MenuToLinksIdDTOInterface $id): MenuToLinksInterface
     {
 
-        $idFields = MenuToLinksIdDTOInterface::ID_KEYS;
-        $counter = 0;
-        $placeHolders = [];
-        $params = [];
-
-        foreach ($idFields as $field) {
-            $counter++;
-            $params[] = $id->getData($field);
-            $placeHolders[] = $field . ' = $' . $counter;
-        }
+        [$placeHolders, $params] = $this->getParametersFromIdDto($id);
 
         $query = 'SELECT ' . $this->table . '.* FROM ' . $this->table . ' WHERE '
-            . implode(' AND ', $placeHolders) . '; = $1';
+            . implode(' AND ', $placeHolders) . ';';
 
         $arr = $this->database->queryParams($query, $params);
 
@@ -59,26 +53,88 @@ class MenuToLinksRepository implements MenuToLinksRepositoryInterface
         return $this->createFromRow($row);
     }
 
-
-
-
-
-
-
+    /** 
+     * 1 to 1 with the Repository class
+     */
     public function list(string $expression, array $params): array
     {
+        $entities = [];
+
+        $query = 'SELECT ' . $this->table . '.* FROM ' . $this->table . ' ' . $expression;
+        $arr = $this->database->queryParams($query, $params);
+        foreach ($arr as $row) {
+            $entities[] = $this->createFromRow($row);
+        }
+
+        return $entities;
     }
 
+    /** 
+     * 1 to 1 with the Repository class
+     */
     public function add(MenuToLinksInterface $model): MenuToLinksInterface
     {
+        $keys = [];
+        $values = [];
+        $params = [];
+        $count = 0;
+        foreach ($model->getAllData() as $key => $value) {
+            $count++;
+            $params[] = '$' . $count;
+            $keys[] = $key;
+            $values[] = $value;
+        }
+
+        $query = 'INSERT INTO ' . $this->table . ' (' . implode(', ', $keys) . ') VALUES ('
+            . implode(', ', $params) . ') RETURNING *';
+        try {
+            $arr = $this->database->queryParams($query, $values);
+            $row = $arr[0];
+            return $this->createFromRow($row);
+        } catch (QueryExeption $e) {
+            throw new CouldNotAddException($e->getMessage());
+        }
     }
 
     public function deleteById(MenuToLinksIdDTOInterface $id): void
     {
+
+        [$placeHolders, $params] = $this->getParametersFromIdDto($id);
+
+        $query = 'DELETE FROM ' . $this->table . ' WHERE '
+            . implode(' AND ', $placeHolders);
+        try {
+            $this->database->queryParams($query, $params);
+        } catch (QueryExeption $e) {
+            throw new CouldNotDeleteException ($e->getMessage());
+        }
     }
 
     public function save(MenuToLinksInterface $model): MenuToLinksInterface
     {
+        // prepare id
+        $id = $model->getId();
+        $counter = count($id->getAllData());
+        [$IdPlaceHolders, $params] = $this->getParametersFromIdDto($id);
+
+        // prepare all fields
+        $fields = [];
+        foreach ($model->getAllData() as $key => $value) {
+            $counter++;
+            $fields[] = $key . ' = $' . $counter;
+            $params[] = $value;
+        }
+
+        $query = 'UPDATE ' . $this->table . ' SET ' . implode(', ', $fields)
+            . 'WHERE ' . implode(' AND ', $IdPlaceHolders) . ' RETURNING *';
+
+        try {
+            $arr = $this->database->queryParams($query, $params);
+            $row = $arr[0];
+            return $this->createFromRow($row);
+        } catch (QueryExeption $e) {
+            throw new CouldNotSaveException($e->getMessage());
+        }
     }
 
     /**
@@ -103,5 +159,30 @@ class MenuToLinksRepository implements MenuToLinksRepositoryInterface
 
         // 3 job is done
         return $entity;
+    }
+
+    /**
+     * Prepare id data for the query
+     * Used in methods: 
+     *   - getById
+     *   - deleteById
+     * 
+     * @param MenuToLinksIdDTOInterface $id
+     * @return array [ ['fieldname = $1', ...], [$value, ...] ]
+     */
+    protected function getParametersFromIdDto(MenuToLinksIdDTOInterface $id): array
+    {
+        $idFields = MenuToLinksIdDTOInterface::ID_KEYS;
+        $counter = 0;
+        $placeHolders = [];
+        $params = [];
+
+        foreach ($idFields as $field) {
+            $counter++;
+            $params[] = $id->getData($field);
+            $placeHolders[] = $field . ' = $' . $counter;
+        }
+
+        return [$placeHolders, $params];
     }
 }
