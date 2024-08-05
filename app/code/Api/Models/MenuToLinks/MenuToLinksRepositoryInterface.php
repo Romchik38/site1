427c67e10f8f86b41af1fc1d36581c1cbe205cb2
Romchik38 <pomahehko.c@gmail.com>
2024-08-05 20:07:23 +0300

    refactor

--- app/code/Api/Models/MenuToLinks/MenuToLinksRepositoryInterface.php
+++ app/code/Api/Models/MenuToLinks/MenuToLinksRepositoryInterface.php
@@ -4,61 +4,9 @@ declare(strict_types=1);
 
 namespace Romchik38\Site1\Api\Models\MenuToLinks;
 
-use Romchik38\Server\Models\Errors\CouldNotSaveException;
-use Romchik38\Server\Models\Errors\CouldNotDeleteException;
-use Romchik38\Server\Models\Errors\CouldNotAddException;
-use Romchik38\Server\Models\Errors\NoSuchEntityException;
+use Romchik38\Server\Api\Models\CompositeId\CompositeIdRepositoryInterface;
 
-interface MenuToLinksRepositoryInterface
+interface MenuToLinksRepositoryInterface extends CompositeIdRepositoryInterface
 {
-    /**
-     * Create a new empty entity
-     *   
-     * @return MenuToLinksInterface
-     */
-    public function create(): MenuToLinksInterface;
-
-    /**
-     * Search in the repository by provided id
-     * 
-     * @param MenuToLinksIdDTOInterface $id
-     * @throws NoSuchEntityException
-     * @return MenuToLinksInterface
-     */
-    public function getById(MenuToLinksIdDTOInterface $id): MenuToLinksInterface;
-
-    /**
-     * Search in the repository by provided condition
-     * 
-     * @param string $expression [condition]
-     * @param array $params
-     * @return MenuToLinksInterface[]
-     */
-    public function list(string $expression, array $params): array;
-
-    /**
-     * Add new entity
-     * 
-     * @param MenuToLinksInterface $model [a new entity to save]
-     * @throws CouldNotAddException
-     * @return MenuToLinksInterface
-     */
-    public function add(MenuToLinksInterface $model): MenuToLinksInterface;
-
-    /**
-     * Delete existing entity by provided id
-     * 
-     * @throws CouldNotDeleteException  
-     * @return void
-     */
-    public function deleteById(MenuToLinksIdDTOInterface $id): void;
-
-    /**
-     * Save existing model
-     *
-     * @param MenuToLinksInterface $model
-     * @throws CouldNotSaveException
-     * @return ModelInterface
-     */
-    public function save(MenuToLinksInterface $model): MenuToLinksInterface;
+   
 }
