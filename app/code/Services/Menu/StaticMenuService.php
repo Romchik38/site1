a2559de9362142128628ee7c881f1d985dd0ee36
Romchik38 <pomahehko.c@gmail.com>
2024-08-08 20:22:26 +0300

    creating linkDTO list

--- app/code/Services/Menu/StaticMenuService.php
+++ app/code/Services/Menu/StaticMenuService.php
@@ -16,6 +16,8 @@ use Romchik38\Site1\Api\Models\MenuToLinks\MenuToLinksInterface;
 use Romchik38\Site1\Api\Models\MenuToLinks\MenuToLinksRepositoryInterface;
 use Romchik38\Site1\Api\Models\Virtual\Link\VirtualLinkRepositoryInterface;
 use Romchik38\Site1\Services\Errors\Menu\CouldNotCreateMenu;
+use Romchik38\Site1\Api\Models\Virtual\Link\VirtualLinkInterface;
+use Romchik38\Site1\Api\Models\DTO\Menu\LinkDTOInterface;
 
 class StaticMenuService extends StaticMenuServiceInterface
 {
@@ -29,8 +31,7 @@ class StaticMenuService extends StaticMenuServiceInterface
         protected MenuToLinksRepositoryInterface $menuToLinksRepository,
         protected MenuLinksRepositoryInterface $menuLinksRepository,
         protected VirtualLinkRepositoryInterface $virtualLinkRepository
-    ) {
-    }
+    ) {}
 
     /**
      * @throws CouldNotCreateMenu 
@@ -60,19 +61,102 @@ class StaticMenuService extends StaticMenuServiceInterface
     /**
      * Return array of link entities
      * 
+     * @throws CouldNotCreateMenu
      * @return LinkDTOInterface[]
      */
     protected function getLinkDTOs(MenuModelInterface $menu): array
     {
-
         $linkModels = $this->virtualLinkRepository->getByMenuId($menu->getId());
 
-        $noDependecies = [];
-        $dependencies = [];
-        // 1 dto wothout dependencies
-        foreach ($linkModels as $linkModel) {
-            if ($model->getLinkId() === $linkModel->getParentLinkId()) {
+        /** @var array $dep [linkId => [childLinkId, ...], ...] */
+        $dep = $this->prepareDep($linkModels);
+        $hash = [];
+        // move from $dep to $hash
+        $isCercular = false;
+        while ($isCercular === false) {
+            $isCercular = true;
+            [$updatedHaveDep, $updatedHash] = createLink($dep, $hash, $linkModels);
+            if (count($updatedHash) > count($hash)) {
+                $isCercular = false;
+                $hash = $updatedHash;
+                $dep = $updatedHaveDep;
             }
         }
+        // 3 check on cercular
+        if (count($linkModels) !== count($hash)) {
+            throw new CouldNotCreateMenu('There are cercular dependencies in the menu: ' . $menu->getId());
+        }
+
+        // 4 create a list of LinkDTOInterface
+        $linkList = [];
+        /** @var LinkDTOInterface $elem*/
+        foreach ($hash as $elem) {
+            if ($elem->getParentLinkId() === 0) {
+                $linkList[] = $elem;
+            }
+        }
+        return $linkList;
+    }
+
+    /**
+     * Uses initial data ( $data ) to create a temporary hash table "from" ( $dep )
+     * In createLink() this data ( $dep ) will migrate to table "to" ( $hash )
+     */
+    protected function prepareDep(array $data): array
+    {
+        $dep = [];
+        $counter = 0;
+        while ($counter < count($data)) {
+            /** @var VirtualLinkInterface $elem */
+            $elem = $data[$counter];
+            /** @var VirtualLinkInterface $innerElem */
+            foreach ($data as $innerElem) {
+                if ($elem->getLinkId() === $innerElem->getLinkId()) {
+                    continue;
+                }
+                if ($elem->getLinkId() === $innerElem->getParentLinkId()) {
+                    $innerDep = $dep[$elem->getLinkId()] ?? [];
+                    array_push($innerDep, $innerElem->getLinkId());
+                    $dep[$elem->getLinkId()] = $innerDep;
+                }
+            }
+            if (key_exists($elem->getLinkId(), $dep) === false) {
+                $dep[$elem->getLinkId()] = [];
+            }
+            $counter++;
+        }
+        return $dep;
+    }
+
+    protected function createLink($haveDep, $hash, $inititalData)
+    {
+        $newHaveDep = [];
+        foreach ($haveDep as $linkId => $depArr) {
+            // check in nodep
+            $isReady = true;
+            $depInstancies = [];
+            foreach ($depArr as $depLinkId) {
+                if (key_exists($depLinkId, $hash) === false) {
+                    $isReady = false;
+                } else {
+                    $depInstancies[] = $hash[$depLinkId];
+                }
+            }
+            if ($isReady === false) {
+                // check on cercular
+                $newHaveDep[$linkId] = $depArr;
+                continue;
+            }
+            // create
+            /** @var  $innerElem */
+            foreach ($inititalData as $innerElem) {
+                if ($innerElem['link_id'] === $linkId) {
+                    $link = new Link($linkId, $innerElem['parrent_link_id'], $depInstancies);
+                    $hash[$link->linkId] = $link;
+                }
+            }
+        }
+
+        return [$newHaveDep, $hash];
     }
 }
