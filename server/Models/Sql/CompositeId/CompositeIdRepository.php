7911561565ccd767f1a6ced348e3cec67681b2ae
Romchik38 <pomahehko.c@gmail.com>
2024-08-12 18:48:05 +0300

    refactor

--- server/Models/Sql/CompositeId/CompositeIdRepository.php
+++ server/Models/Sql/CompositeId/CompositeIdRepository.php
@@ -31,49 +31,6 @@ class CompositeIdRepository implements CompositeIdRepositoryInterface
     ) {
     }
 
-    public function create(): CompositeIdModelInterface
-    {
-        return $this->modelFactory->create();
-    }
-
-    public function getById(CompositeIdDTOInterface $id): CompositeIdModelInterface
-    {
-
-        [$placeHolders, $params] = $this->getParametersFromIdDto($id);
-
-        $query = 'SELECT ' . $this->table . '.* FROM ' . $this->table . ' WHERE '
-            . implode(' AND ', $placeHolders);
-
-        $arr = $this->database->queryParams($query, $params);
-
-        if (count($arr) === 0) {
-            throw new NoSuchEntityException('row with complex id ' . implode(', ', $params)
-                . ' do not present in the ' . $this->table . ' table');
-        }
-        $row = $arr[0];
-
-        return $this->createFromRow($row);
-    }
-
-    /** 
-     * 1 to 1 with the Repository class
-     */
-    public function list(string $expression, array $params): array
-    {
-        $entities = [];
-
-        $query = 'SELECT ' . $this->table . '.* FROM ' . $this->table . ' ' . $expression;
-        $arr = $this->database->queryParams($query, $params);
-        foreach ($arr as $row) {
-            $entities[] = $this->createFromRow($row);
-        }
-
-        return $entities;
-    }
-
-    /** 
-     * 1 to 1 with the Repository class
-     */
     public function add(CompositeIdModelInterface $model): CompositeIdModelInterface
     {
         $keys = [];
@@ -98,6 +55,30 @@ class CompositeIdRepository implements CompositeIdRepositoryInterface
         }
     }
 
+    public function create(): CompositeIdModelInterface
+    {
+        return $this->modelFactory->create();
+    }
+
+    public function getById(CompositeIdDTOInterface $id): CompositeIdModelInterface
+    {
+
+        [$placeHolders, $params] = $this->getParametersFromIdDto($id);
+
+        $query = 'SELECT ' . $this->table . '.* FROM ' . $this->table . ' WHERE '
+            . implode(' AND ', $placeHolders);
+
+        $arr = $this->database->queryParams($query, $params);
+
+        if (count($arr) === 0) {
+            throw new NoSuchEntityException('row with complex id ' . implode(', ', $params)
+                . ' do not present in the ' . $this->table . ' table');
+        }
+        $row = $arr[0];
+
+        return $this->createFromRow($row);
+    }
+
     public function deleteById(CompositeIdDTOInterface $id): void
     {
 
@@ -110,6 +91,19 @@ class CompositeIdRepository implements CompositeIdRepositoryInterface
         } catch (QueryExeption $e) {
             throw new CouldNotDeleteException($e->getMessage());
         }
+    }
+
+    public function list(string $expression, array $params): array
+    {
+        $entities = [];
+
+        $query = 'SELECT ' . $this->table . '.* FROM ' . $this->table . ' ' . $expression;
+        $arr = $this->database->queryParams($query, $params);
+        foreach ($arr as $row) {
+            $entities[] = $this->createFromRow($row);
+        }
+
+        return $entities;
     }
 
     public function save(CompositeIdModelInterface $model): CompositeIdModelInterface
