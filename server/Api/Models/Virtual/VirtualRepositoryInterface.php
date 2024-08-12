f4d3c113a577820689d31f3aa9df48d9f44a5b76
Romchik38 <pomahehko.c@gmail.com>
2024-08-12 21:41:34 +0300

    refactor

--- server/Api/Models/Virtual/VirtualRepositoryInterface.php
+++ server/Api/Models/Virtual/VirtualRepositoryInterface.php
@@ -8,7 +8,16 @@ use Romchik38\Server\Api\Models\ModelInterface;
 
 interface VirtualRepositoryInterface
 {
+    /**
+     * Create an empty entity
+     * 
+     * @return ModelInterface
+     */
+    public function create(): ModelInterface;
+
     /** 
+     * Search entities by provided conditions ( expression and params )
+     * 
      * @return ModelInterface[]
      */
     public function list(string $expression, array $params): array;
