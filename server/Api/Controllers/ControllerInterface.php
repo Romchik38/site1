62d346b853a57ca42816ba2e514bacde2d93c24f
Romchik38 <pomahehko.c@gmail.com>
2024-08-18 14:32:02 +0300

    refactor

--- server/Api/Controllers/ControllerInterface.php
+++ server/Api/Controllers/ControllerInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Romchik38\Server\Api\Controllers;
 
-use Romchik38\Server\Api\Results\ControllerResultInterface;
-
 interface ControllerInterface
 {
     /** transfers control to next controller */
