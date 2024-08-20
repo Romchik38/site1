e43ae94a3d4d6d8b1057c6c9132f4fd2b1e1ed54
Romchik38 <pomahehko.c@gmail.com>
2024-08-20 11:36:40 +0300

    is done

--- app/code/Router/Http/RouterHeaders/Changepassword.php
+++ app/code/Router/Http/RouterHeaders/Changepassword.php
@@ -6,22 +6,19 @@ use Romchik38\Server\Api\Results\Http\HttpRouterResultInterface;
 use Romchik38\Server\Api\Router\Http\RouterHeadersInterface;
 use Romchik38\Site1\Api\Services\RequestInterface;
 
-class Changepassword implements RouterHeadersInterface {
+class Changepassword implements RouterHeadersInterface
+{
     public function setHeaders(HttpRouterResultInterface $result, array $path): void
     {
-        $action = $path[count($path) - 1];
         $encodedMessage = urlencode($result->getResponse());
-        $arr = [
-            'index' => '/login/changepassword?' . RequestInterface::MESSAGE_FIELD . '=' . $encodedMessage
-        ];
-        $url = $arr[$action] ?? '';
-        if ($url !== '') {
-            $result->setHeaders([
-                [
-                    'Location: ' . $_SERVER['REQUEST_SCHEME'] . '://'
-                        . $_SERVER['HTTP_HOST'] . $url, true, 301
-                ]
-            ]);
-        }
+        $url = '/login/changepassword?' . RequestInterface::MESSAGE_FIELD . '=' . $encodedMessage;
+        $result->setHeaders([
+            [
+                'Location: ' . $_SERVER['REQUEST_SCHEME'] . '://'
+                    . $_SERVER['HTTP_HOST'] . $url,
+                true,
+                301
+            ]
+        ]);
     }
 }
