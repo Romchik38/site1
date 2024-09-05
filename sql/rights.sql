9874e3628e5a2d4b86da77767bb52938d9d7be7b
Romchik38 <pomahehko.c@gmail.com>
2024-09-05 17:44:45 +0300

    refactor

--- sql/rights.sql
+++ sql/rights.sql
@@ -5,15 +5,15 @@ GRANT connect on database site1 to "service";
 GRANT select on table pages to "service";
 GRANT insert on table pages to "service";
 
-GRANT select on table redirects to "php-fpm";
+GRANT select on table redirects to "service";
 --
-GRANT select on table entities to "php-fpm";
-GRANT select on table entity_field to "php-fpm";
+GRANT select on table entities to "service";
+GRANT select on table entity_field to "service";
 
 
-GRANT select, insert, update on table users to "php-fpm";
-GRANT USAGE, SELECT ON SEQUENCE users_user_id_seq TO "php-fpm";
+GRANT select, insert, update on table users to "service";
+GRANT USAGE, SELECT ON SEQUENCE users_user_id_seq TO "service";
 
-GRANT select, insert, update on table recovery_email to "php-fpm";
+GRANT select, insert, update on table recovery_email to "service";
 
-GRANT select, insert, update, delete on table menu, menu_links, menu_to_links to "php-fpm";
+GRANT select, insert, update, delete on table menu, menu_links, menu_to_links to "service";
