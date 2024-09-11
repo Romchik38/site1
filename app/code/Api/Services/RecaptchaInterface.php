13e90c215b144228919cfa33a427829733d27954
Romchik38 <pomahehko.c@gmail.com>
2024-09-11 18:12:34 +0300

    recapcha service in progress

---
+++ app/code/Api/Services/RecaptchaInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Site1\Api\Services;
+
+use Romchik38\Server\Api\Models\DTO\DTOInterface;
+
+interface RecaptchaInterface
+{
+    public function check(string $actionName): bool;
+    public function getRecaptchaDTO(string $actionName): DTOInterface;
+}
