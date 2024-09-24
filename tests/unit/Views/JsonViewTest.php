de3c7fd85df80a4254efaba6a25259305ebb182a
Romchik38 <pomahehko.c@gmail.com>
2024-09-24 20:22:49 +0300

    tests for  server are ready

--- tests/unit/Views/JsonViewTest.php
+++ tests/unit/Views/JsonViewTest.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Romchik38\Server\Views\JsonView;
 use Romchik38\Server\Models\DTO\DefaultView\DefaultViewDTO;
+use Romchik38\Server\Views\Errors\CantCreateViewException;
 
 class JsonViewTest extends TestCase
 {
+    /** 
+     * success
+     */
     public function testToString()
     {
         $name = 'some name';
@@ -19,5 +23,37 @@ class JsonViewTest extends TestCase
         $result = $jsonView->toString();
 
         $this->assertSame(json_encode($dto->getAllData()), $result);
+    }
+
+    /**
+     * to early access
+     */
+    public function testToStringThrowsToEarly()
+    {
+        $this->expectException(CantCreateViewException::class);
+        $this->expectExceptionMessage(JsonView::class . ': Controller data was not set');
+
+        $jsonView = new JsonView();
+        $jsonView->toString();
+    }
+
+    /**
+     * json_encode return false
+     */
+    public function testToStringThrowsErrorEncoding()
+    {
+        $dto = new class('', '') extends DefaultViewDTO {
+            public function __construct(string $name, string $description)
+            {
+                $this->data['error_key'] = NAN;
+            }
+        };
+
+        $this->expectException(CantCreateViewException::class);
+        $this->expectExceptionMessage(JsonView::class . ': error while encoding data to json');
+
+        $jsonView = new JsonView();
+        $jsonView->setControllerData($dto);
+        $result = $jsonView->toString();
     }
 }
